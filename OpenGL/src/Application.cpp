9d06a3e606d53df81017ac787a3d41b45fde345b
minoring <minho1350@gmail.com>
2020-02-08 00:14:33 -0800

    GLEW for openGL function calls

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -1,3 +1,4 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
 
@@ -17,6 +18,12 @@ int main(void) {
   /* Make the window's context current */
   glfwMakeContextCurrent(window);
 
+  if (glewInit() != GLEW_OK) {
+    std::cout << "Error!" << std::endl;
+  }
+
+  std::cout << glGetString(GL_VERSION) << std::endl;
+
   /* Loop until the user closes the window */
   while (!glfwWindowShouldClose(window)) {
     /* Render here */
@@ -24,8 +31,8 @@ int main(void) {
 
     glBegin(GL_TRIANGLES);
     glVertex2f(-0.5f, -0.5f);
-    glVertex2f( 0.0f,  0.5f);
-    glVertex2f( 0.5f, -0.5f);
+    glVertex2f(0.0f, 0.5f);
+    glVertex2f(0.5f, -0.5f);
     glEnd();
 
     /* Swap front and back buffers */
