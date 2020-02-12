fa133180e51ac2c6f95a7ae32b7752882e7dc423
minoring <minho1350@gmail.com>
2020-02-11 21:27:41 -0800

    Shader to file (stringstream)

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -1,7 +1,38 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <fstream>
 #include <iostream>
+#include <sstream>
 #include <string>
+
+struct ShaderProgramSource {
+  std::string VertexSource;
+  std::string FragmentSource;
+};
+
+static ShaderProgramSource ParseShader(const std::string &filepath)
+{
+  std::ifstream stream(filepath);
+
+  enum class ShaderType { None = -1, VERTEX = 0, FRAGMENT = 1 };
+
+  std::string line;
+  std::stringstream ss[2];
+  ShaderType type = ShaderType::None;
+
+  while (getline(stream, line)) {
+    if (line.find("#shader") != std::string::npos) {
+      if (line.find("vertex") != std::string::npos)
+        type = ShaderType::VERTEX;
+      else if (line.find("fragment") != std::string::npos)
+        type = ShaderType::FRAGMENT;
+    } else {
+      ss[(int)type] << line << '\n';
+    }
+  }
+
+  return {ss[0].str(), ss[1].str()};
+}
 
 static unsigned int CompileShader(unsigned int type, const std::string &source)
 {
@@ -75,21 +106,11 @@ int main(void)
   glGenBuffers(1, &buffer);
   glBindBuffer(GL_ARRAY_BUFFER, buffer);
   glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
-  std::string vertexShader = 
-  "#version 120\n"
-  "attribute vec4 position;\n"
-  "void main()"
-  "{"
-    "gl_Position = position;"
-  "}";
-  std::string fragmentShader = 
-  "#version 120\n"
-  "void main()"
-  "{"
-    "gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);"
-  "}";
 
-  unsigned int shader = CreateShader(vertexShader, fragmentShader);
+  ShaderProgramSource source = ParseShader("src/res/shaders/Basic.shader");
+
+  unsigned int shader =
+      CreateShader(source.VertexSource, source.FragmentSource);
   glUseProgram(shader);
 
   int positionLoc = glGetAttribLocation(shader, "position");
