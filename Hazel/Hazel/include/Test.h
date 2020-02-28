7c287da198b76770284087d46189522d4fb4099f
minoring <minho1350@gmail.com>
2020-02-27 21:50:18 -0800

    Setup

---
+++ Hazel/Hazel/include/Test.h
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace hazel {
+
+void Print();
+
+} // namespace hazel
---
+++ Hazel/Hazel/src/Test.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+
+#include "Test.h"
+
+namespace hazel {
+
+void Print() {
+    printf("Hello world\n");
+}
+
+} // namespace hazel
