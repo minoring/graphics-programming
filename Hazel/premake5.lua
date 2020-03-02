a075423a9faafe696acd63cdf3bb7dd81d0d5259
minoring <minho1350@gmail.com>
2020-03-01 19:41:31 -0800

    Premake

---
+++ Hazel/premake5.lua
@@ -0,0 +1,113 @@
+workspace "Hazel"
+    architecture "x64"
+
+    configurations
+    {
+        "Debug",
+        "Release",
+        "Dist"
+    }
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Hazel"
+    location "Hazel"
+    kind "SharedLib"
+    language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "%{prj.name}/include/**.h",
+        "%{prj.name}/src/**.cpp"
+    }
+
+    includedirs
+    {
+        "%{prj.name}/include/**.h",
+        "%{prj.name}/vendor/spdlog/include"
+    }
+
+    filter "system:windows"
+        cppdialect "C++17"
+        staticruntime "On"
+        systemversion "latest"
+
+        defines
+        {
+            "HZ_PLATFORM_WINDOWS",
+            "HZ_BUILD_DLL"
+        }
+
+        postbuildcommands
+        {
+            ("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+        }
+
+    filter "configurations:Debug"
+        defines "HZ_DEBUG"
+        symbols "On"
+
+    filter "configurations:Debug"
+        defines "HZ_RELEASE"
+        optimize "On"
+
+    filter "configurations:Dist"
+        defines "HZ_DIST"
+        optimize "On"
+
+project "Sandbox"
+    location "Sandbox"
+    kind "ConsoleApp"
+
+    language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "%{prj.name}/include/**.h",
+        "%{prj.name}/src/**.cpp"
+    }
+
+    includedirs
+    {
+        "%{prj.name}/vendor/spdlog/include",
+        "Hazel/include"
+    }
+
+    links
+    {
+        "Hazel"
+    }
+
+    filter "system:windows"
+        cppdialect "C++17"
+        staticruntime "On"
+        systemversion "latest"
+
+        defines
+        {
+            "HZ_PLATFORM_WINDOWS",
+            "HZ_BUILD_DLL"
+        }
+
+        postbuildcommands
+        {
+            ("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+        }
+
+    filter "configurations:Debug"
+        defines "HZ_DEBUG"
+        symbols "On"
+
+    filter "configurations:Release"
+        defines "HZ_RELEASE"
+        optimize "On"
+
+    filter "configurations:Dist"
+        defines "HZ_DIST"
+        optimize "On"
